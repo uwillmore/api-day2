0fb5970db92ee0e3c4aa78ac63fd5e7ef38d179c
David R. Jones <drj@rackfx.com>
2015-11-18 20:35:34 -0700

    Ouath get calendars available to user

--- get_token.php
+++ get_token.php
@@ -1,13 +1,11 @@
 <?php
   $url = "https://accounts.google.com/o/oauth2/auth";
 
-
-
   $params = array(
     "response_type" => "code",
     "client_id" => "733929298649-a32ahoj5jio55csf1svs3et5sftvgi4n.apps.googleusercontent.com",
     "redirect_uri" => "http://localhost/oauth2callback.php",
-    "scope" => "https://www.googleapis.com/auth/plus.me"
+    "scope" => "https://www.google.com/calendar/feeds/"
   );
 
   $request_url = $url . "?". http_build_query($params);
